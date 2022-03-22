58b17152bdee13c714d9940cbb3323505318dfa0
Angela Petrone <apetrone@hawk.iit.edu>
2022-03-22 13:14:48 -0500

    Implement localStorage

--- js/site.js
+++ js/site.js
@@ -89,3 +89,161 @@ if (newBillingFieldset !== null) {
     }
   });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------LOCAL STORAGE---------------//
+
+var page = document.querySelector('html');
+
+
+// form submission button
+function submitForm(event) {
+  var targetElement = event.target;
+  event.preventDefault();
+  writeFormDataToLocalStorage(targetElement.name); // STORE all the form data
+  window.location.href = targetElement.action;
+}
+
+
+// create new local storage object or find existing one
+function findOrCreateLocalStorageObject(keyName) {
+  var jsObject = readJsonFromLocalStorage(keyName);
+
+  if (Object.keys(jsObject).length === 0) {
+    writeJsonToLocalStorage(keyName, jsObject);
+  }
+  return jsObject;
+}
+
+
+// read from local storage
+function readJsonFromLocalStorage(keyName) {
+  var jsonObject = localStorage.getItem(keyName);
+  var jsObject = {};
+
+  if (jsonObject) {
+    try {
+      jsObject = JSON.parse(jsonObject);
+    } catch(e) {
+      console.error(e);
+      jsObject = {};
+    }
+  }
+
+  return jsObject;
+}
+
+
+// write to local storage
+function writeJsonToLocalStorage(keyName, jsObject) {
+  localStorage.setItem(keyName, JSON.stringify(jsObject));
+}
+
+
+function writeFormDataToLocalStorage(formName, inputElement) {
+  var formData = findOrCreateLocalStorageObject(formName);
+
+  // Set just a single input value
+  if (inputElement) {
+    formData[inputElement.name] = inputElement.value;
+  } else {
+    // Set all form input values, e.g., on a submit event
+    var formElements = document.forms[formName].elements;
+    for (var i = 0; i < formElements.length; i++) {
+      // Don't store empty elements, like the submit button
+      if (formElements[i].value !== "") {
+        formData[formElements[i].name] = formElements[i].value;
+      }
+    }
+  }
+
+  writeJsonToLocalStorage(formName, formData);
+}
+
+
+
+//remove element from local storage
+function destroyFormDataInLocalStorage(formName) {
+  localStorage.removeItem(formName);
+}
+
+
+// recover info from local storage
+function restoreFormDataFromLocalStorage(formName) {
+  var jsObject = readJsonFromLocalStorage(formName);
+  var formValues = Object.entries(jsObject);
+  if (formValues.length === 0) {
+    return; // nothing to restore
+  }
+
+
+
+
+//index.html login page
+if(page.id === 'login'){
+  var form = document.querySelector("form");
+  restoreFormDataFromLocalStorage(form.name);
+  form.addEventListener('submit', submitForm);
+}
+
+
+//shipping
+if(page.id === 'shipping'){
+  var form = document.querySelector("form");
+  restoreFormDataFromLocalStorage(form.name);
+  form.addEventListener('submit', submitForm);
+}
+
+//payment
+if(page.id === 'payment'){
+  var form = document.querySelector("form");
+  restoreFormDataFromLocalStorage(form.name);
+  form.addEventListener('submit', submitForm);
+}
+
+//billing
+if(page.id === 'billing'){
+  var form = document.querySelector("form");
+  restoreFormDataFromLocalStorage(form.name);
+  form.addEventListener('submit', submitForm);
+}
+
+
+
+
+
+
+
+
+
+
+
+// create error if credit card expiration year is before 2022
+var errorClass = targetElement.name + '-error';
+var errorEl = document.querySelector('.' + errorClass);
+
+  if (targetElement.value.lenght < 3) {
+    // Don't add duplicate errors
+    if (!errorEl) {
+      var errorText = 'CVC must be 3 numbers long';
+      errorEl = document.createElement('p');
+      errorEl.className = errorClass
+      errorEl.innerText = errorText;
+      targetElement.before(errorEl);
+    }
+  } else {
+    if (errorEl) {
+      errorEl.remove();
+    }
+  }
+}

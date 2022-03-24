9f58a63551bb31bb20c3c394c66a2bbe94991ce1
Angela Petrone <apetrone@hawk.iit.edu>
2022-03-23 21:17:17 -0500

    Add login and delivery page related tests

---
+++ tests/form.js
@@ -0,0 +1,44 @@
+describe ('Login Page', function() {
+
+    it('the form should exist', function(browser) {
+      browser
+        .url('http://localhost:8080/')
+        .assert
+          .elementPresent('form[name="login"]');
+    });
+
+    it('if the form is empty it is not submittable', function(browser) {
+      browser
+        .url('http://localhost:8080/')
+        .click('button.forward')
+        .assert
+          .urlEquals('http://localhost:8080/');
+
+    });
+
+});
+
+describe ('Login Page', function() {
+
+  it('the added delivery cost should match the user preference (regular)', function(browser) {
+    browser
+      .url('http://localhost:8080/delivery')
+      .click('input#regular')
+      .assert.containsText('#shippingCost', 'Delivery: $7')
+  });
+
+  it('the added delivery cost should match the user preference (fast)', function(browser) {
+    browser
+      .url('http://localhost:8080/delivery')
+      .click('input#fast')
+      .assert.containsText('#shippingCost', 'Delivery: $12')
+  });
+
+  it('the added delivery cost should match the user preference (express)', function(browser) {
+    browser
+      .url('http://localhost:8080/delivery')
+      .click('input#express')
+      .assert.containsText('#shippingCost', 'Delivery: $15')
+  });
+
+});
